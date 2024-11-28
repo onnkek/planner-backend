d932789bc0b16952509f4f2135fddf961649b0c4
onnkek <onnkek@gmail.com>
2024-11-28 09:51:10 +0500

    add PUT and DELETE methods

--- src/index.ts
+++ src/index.ts
@@ -10,94 +10,65 @@ interface NoteType {
   visible: boolean
 }
 
+interface TaskType {
+  id: number,
+  body: string,
+  create: string,
+  remove: string,
+  timeleft: string,
+  deadline: string,
+  link: string,
+  visible: boolean,
+  badges: number[]
+}
+
+interface BadgeType {
+  id: number,
+  color: number,
+  text: string
+}
+
+interface DBType {
+  tasks: TaskType[],
+  notes: NoteType[],
+  badges: BadgeType[]
+}
+
 const app = express()
 const port = 8000
 const swaggerFile = JSON.parse(fs.readFileSync('./swagger/output.json').toString())
 
-const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'utf-8'))
+const db: DBType = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'utf-8'))
 
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(express.json());
 
+
+
+
 app.get('/tasks', (req, res) => {
-  // описание роута
   // #swagger.description = 'Get all active tasks'
-  // возвращаемый ответ
   /* #swagger.responses[200] = {
-      // описание ответа
-      description: 'Array of all tasks',
-      // схема ответа - ссылка на модель
-      schema: { $ref: '#/definitions/Tasks' }
-  } */
+           description: 'Get all tasks.',
+           schema: { $ref: '#/definitions/Tasks' }
+   } */
 
   res.send(db.tasks)
 
 })
 
-app.get('/notes', (req, res) => {
-  // описание роута
-  // #swagger.description = 'Get all notes'
-  // возвращаемый ответ
+app.get('/tasks/:id', (req, res) => {
   /* #swagger.responses[200] = {
-      // описание ответа
-      description: 'Array of all notes',
-      // схема ответа - ссылка на модель
-      schema: { $ref: '#/definitions/Notes' }
-  } */
+           description: 'Get a specific task.',
+           schema: { $ref: '#/definitions/Task' }
+   } */
 
-  res.send(db.notes)
-
-})
-
-app.get('/badges', (req, res) => {
-  // описание роута
-  // #swagger.description = 'Get all notes'
-  // возвращаемый ответ
-  /* #swagger.responses[200] = {
-      // описание ответа
-      description: 'Array of all notes',
-      // схема ответа - ссылка на модель
-      schema: { $ref: '#/definitions/Badges' }
-  } */
-
-  res.send(db.badges)
-
-})
-
-app.post('/notes', (req, res) => {
-  /*  #swagger.auto = false
-
-            #swagger.path = '/notes'
-            #swagger.method = 'post'
-            #swagger.description = 'Endpoint added note.'
-            #swagger.produces = ["application/json"]
-            #swagger.consumes = ["application/json"]
-        */
-
-  /*  #swagger.parameters['body'] = {
-          in: 'body',
-          description: 'Body of note.',
-          required: true,
-          schema: {
-                $body: '<p>Test body</p>'
-            }
-      }
-  */
-  if (req.body.body) {
-    const newNote = {
-      "id": Math.random(),
-      "body": req.body.body,
-      "create": Date.now(),
-      "visible": true
-    }
-
-    db.notes.push(newNote)
-    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
-    res.status(200).send(db.notes)
+  const task = db.tasks.find(task => task.id === Number(req.params.id))
+  if (task) {
+    res.send(task)
   } else {
-    res.send(400)
+    res.send(404)
   }
-
 
 
 })
@@ -131,10 +102,10 @@ app.post('/tasks', (req, res) => {
       }
   */
   if (req.body.body && req.body.deadline && req.body.link && req.body.badges) {
-    const newTask = {
+    const newTask: TaskType = {
       "id": Math.random(),
       "body": req.body.body,
-      "create": Date.now(),
+      "create": Date.now().toString(),
       "remove": "",
       "timeleft": "",
       "deadline": req.body.deadline,
@@ -152,6 +123,285 @@ app.post('/tasks', (req, res) => {
 
 
 })
+
+app.delete('/tasks/:id', (req, res) => {
+
+  const task = db.tasks.find(task => task.id === Number(req.params.id))
+  if (task) {
+    const index = db.tasks.indexOf(task)
+    db.tasks.splice(index, 1);
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(db.tasks)
+
+  } else {
+    res.send(404)
+  }
+
+})
+
+app.put('/tasks/:id', (req, res) => {
+
+  const task = db.tasks.find(task => task.id === Number(req.params.id))
+  if (task && (req.body.body || req.body.create || req.body.remove || req.body.timeleft || req.body.deadline || req.body.link || req.body.visible || req.body.badges)) {
+
+    if (req.body.body) {
+      task.body = req.body.body
+    }
+    if (req.body.create) {
+      task.create = req.body.create
+    }
+    if (req.body.remove) {
+      task.remove = req.body.remove
+    }
+    if (req.body.timeleft) {
+      task.timeleft = req.body.timeleft
+    }
+    if (req.body.deadline) {
+      task.deadline = req.body.deadline
+    }
+    if (req.body.link) {
+      task.link = req.body.link
+    }
+    if (req.body.visible) {
+      task.visible = req.body.visible
+    }
+    if (req.body.badges) {
+
+      let badges = [];
+      for (let i in req.body.badges) {
+        if (typeof req.body.badges[i] === 'number') {
+          badges.push(req.body.badges[i])
+        } else {
+          res.send(404)
+        }
+      }
+      task.badges = badges
+    }
+
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(task)
+
+  } else {
+    res.send(404)
+  }
+
+})
+
+
+
+
+
+app.get('/notes', (req, res) => {
+  // #swagger.description = 'Get all active notes'
+  /* #swagger.responses[200] = {
+           description: 'Get all notes.',
+           schema: { $ref: '#/definitions/Notes' }
+   } */
+
+  res.send(db.notes)
+
+})
+
+app.get('/notes/:id', (req, res) => {
+  /* #swagger.responses[200] = {
+           description: 'Get a specific note.',
+           schema: { $ref: '#/definitions/Note' }
+   } */
+
+  const note = db.notes.find(note => note.id === Number(req.params.id))
+  if (note) {
+    res.send(note)
+  } else {
+    res.send(404)
+  }
+
+
+})
+
+app.post('/notes', (req, res) => {
+  /*  #swagger.auto = false
+
+            #swagger.path = '/notes'
+            #swagger.method = 'post'
+            #swagger.description = 'Endpoint added note.'
+            #swagger.produces = ["application/json"]
+            #swagger.consumes = ["application/json"]
+        */
+
+  /*  #swagger.parameters['body'] = {
+          in: 'body',
+          description: 'Body of note.',
+          required: true,
+          schema: {
+                $body: '<p>Test body</p>'
+            }
+      }
+  */
+  if (req.body.body) {
+    const newNote: NoteType = {
+      "id": Math.random(),
+      "body": req.body.body,
+      "create": Date.now(),
+      "visible": true
+    }
+
+    db.notes.push(newNote)
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(db.notes)
+  } else {
+    res.send(400)
+  }
+
+
+
+})
+
+app.delete('/notes/:id', (req, res) => {
+
+  const note = db.notes.find(note => note.id === Number(req.params.id))
+  if (note) {
+    const index = db.notes.indexOf(note)
+    db.notes.splice(index, 1);
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(db.notes)
+
+  } else {
+    res.send(404)
+  }
+
+})
+
+app.put('/notes/:id', (req, res) => {
+
+  const note = db.notes.find(note => note.id === Number(req.params.id))
+  if (note && (req.body.body || req.body.create || req.body.visible)) {
+
+    if (req.body.body) {
+      note.body = req.body.body
+    }
+    if (req.body.create) {
+      note.create = req.body.create
+    }
+    if (req.body.visible) {
+      note.visible = req.body.visible
+    }
+
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(note)
+
+  } else {
+    res.send(404)
+  }
+
+})
+
+
+
+
+
+app.get('/badges', (req, res) => {
+  // #swagger.description = 'Get all active badges'
+  /* #swagger.responses[200] = {
+           description: 'Get all badges.',
+           schema: { $ref: '#/definitions/Badges' }
+   } */
+
+  res.send(db.badges)
+
+})
+
+app.get('/badges/:id', (req, res) => {
+  /* #swagger.responses[200] = {
+           description: 'Get a specific badge.',
+           schema: { $ref: '#/definitions/Badge' }
+   } */
+
+  const badge = db.badges.find(badge => badge.id === Number(req.params.id))
+  if (badge) {
+    res.send(badge)
+  } else {
+    res.send(404)
+  }
+
+
+})
+
+app.post('/badges', (req, res) => {
+  /*  #swagger.auto = false
+
+            #swagger.path = '/badges'
+            #swagger.method = 'post'
+            #swagger.description = 'Endpoint added badge.'
+            #swagger.produces = ["application/json"]
+            #swagger.consumes = ["application/json"]
+        */
+
+  /*  #swagger.parameters['body'] = {
+          in: 'body',
+          description: 'Data of badge.',
+          required: true,
+          schema: {
+            "color": 3,
+            "text": "WARNING"
+          }
+      }
+  */
+  if (req.body.text && req.body.color) {
+    const newBadge: BadgeType = {
+      "id": Math.random(),
+      "color": req.body.color,
+      "text": req.body.text
+    }
+
+    db.badges.push(newBadge)
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(db.badges)
+  } else {
+    res.send(400)
+  }
+
+
+})
+
+app.delete('/badges/:id', (req, res) => {
+
+  const badge = db.badges.find(badge => badge.id === Number(req.params.id))
+  if (badge) {
+    const index = db.badges.indexOf(badge)
+    db.badges.splice(index, 1);
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(db.badges)
+
+  } else {
+    res.send(404)
+  }
+
+})
+
+app.put('/badges/:id', (req, res) => {
+
+  const badge = db.badges.find(badge => badge.id === Number(req.params.id))
+  if (badge && (req.body.color || req.body.text)) {
+
+    if (req.body.color) {
+      badge.color = req.body.color
+    }
+    if (req.body.text) {
+      badge.text = req.body.text
+    }
+
+    fs.writeFileSync(path.resolve(__dirname, 'db.json'), JSON.stringify(db), 'utf-8')
+    res.status(200).send(badge)
+
+  } else {
+    res.send(404)
+  }
+
+})
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
